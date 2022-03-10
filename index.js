60c692642cb8e82170a78bd408889c88f204dd11
Ian F <ian.ferg36@gmail.com>
2022-03-10 11:16:12 -0700

    refactored event listener to receive input and push into empty array

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-let saveInputBtn = document.getElementById("input-btn");
-saveInputBtn.addEventListener("click", function () {
-  console.log("the event listener works");
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
 });
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `;
+  }
+  ulEl.innerHTML = listItems;
+}
