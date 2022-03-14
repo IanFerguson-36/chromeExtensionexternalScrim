22c079117bcea0f074dba6ec0c6c1501470d5704
Ian F <ian.ferg36@gmail.com>
2022-03-14 13:52:49 -0600

    save tab button with event listener added

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 let myLeads = [];
-let oldLeads = [];
+
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myleads"));
 const deleteBtn = document.getElementById("delete-btn");
+const saveTabBtn = document.getElementById("save-tab-btn");
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   render(myLeads);
 }
 
+saveTabBtn.addEventListener("click", function () {
+  console.log("save tab clicked!");
+});
+
 function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
               <li>
                   <a target='_blank' href='${leads[i]}'>
