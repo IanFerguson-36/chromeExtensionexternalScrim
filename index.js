b9a03158d21c2f80bb6aa5ad3b55a249b0b0fc22
Ian F <ian.ferg36@gmail.com>
2022-03-14 12:43:06 -0600

    refactored renderleads to render function in order to pass parameters

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let myLeads = [];
+let oldLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
@@ -7,32 +8,32 @@ const deleteBtn = document.getElementById("delete-btn");
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
+}
+
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+              <li>
+                  <a target='_blank' href='${leads[i]}'>
+                      ${leads[i]}
+                  </a>
+              </li>
+          `;
+  }
+  ulEl.innerHTML = listItems;
 }
 
 deleteBtn.addEventListener("click", function () {
   localStorage.clear();
-  myLeads.length = 0;
-  ulEl.innerHTML = null;
+  myLeads = [];
+  render(myLeads);
 });
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
+  render(myLeads);
 });
-
-function renderLeads() {
-  let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
-    listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li>
-        `;
-  }
-  ulEl.innerHTML = listItems;
-}
