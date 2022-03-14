f5234209b4f3689228749395f0c933944038d72c
Ian F <ian.ferg36@gmail.com>
2022-03-14 12:31:54 -0600

    added delete button which clears local storage array and dom

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myleads"));
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myleads"));
+const deleteBtn = document.getElementById("delete-btn");
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   renderLeads();
 }
+
+deleteBtn.addEventListener("click", function () {
+  localStorage.clear();
+  myLeads.length = 0;
+  ulEl.innerHTML = null;
+});
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
